17571c58dc10e1468f4e89f4614c66bf5963f3d1
Rajee <rajeevan@gmail.com>
2023-10-17 15:53:53 +0530

    jsfile

---
+++ script.js
@@ -0,0 +1,202 @@
+//Q1
+//comments can make code readable
+
+//Q2
+//*Welcome to learn Javascript
+
+//Q3
+/*code readable, 
+easy to reuse and informative
+*/
+
+//Q4
+// var a = String;
+var firstName="Rajee"
+
+var x=5;
+var y=4;
+z=x<y;
+console.log( null===undefined)
+console.log( null===undefined)
+
+//Q5
+// String variablemy
+const myName ="Hello WORLD";
+console.log(typeof myName);
+
+//Boolean variables
+const isTrue = true;
+const isFalse = false;
+console.log(typeof isTrue);
+console.log(typeof false);
+
+//Undefined variable
+let secondName;
+console.log(typeof secondName);
+
+//null variable
+const myExample = null;
+console.log (typeof myExample);
+
+//Q6
+let name1;
+var name2
+let name3;
+var name4;
+
+
+//Q7
+let name5= "raja"
+var name6=45
+let name7=true
+var name8="raju"
+
+
+// //Q8van
+// let firstName = 'rajeevan';
+// let LastName = "rajee";
+// let maritalStatus = 'Single';
+// let country = 'srilanka';
+// let Age = 25;
+
+// Q9
+ var [firstName, lastName, maritalstatus,age] =["rajee","rajeevan"]
+
+
+// Q10
+
+const myAeg = 25;
+const yourAge = 21;
+console.log("I" + "years old.");
+console.log("you are"+ yourAge + "years old");
+
+
+
+
+// Day 02 Q
+
+let challenge = 'learn Javascript';
+
+
+//------------------Day 02 Q------------------------------------------------
+
+//Q1
+var j = "Learn JavaScript";
+
+
+//Q2
+console.log(j);
+
+//Q3
+console.log(j.length);
+
+//Q4
+console.log(j.toUpperCase());
+
+//05
+console.log(j.toLowerCase());
+
+
+//Q6
+console.log(j.substring(-1));
+console.log(j.substring(15));
+
+//Q7
+
+let learn = "Days Of JavaScript from learn Javascript";
+console.log(learn.length);
+console.log(learn.substring(19));
+
+//Q8
+
+console.log(learn.includes("hi"))  
+
+//Q9
+let split= 'learn JavaScript';
+let split1 = split.split("");
+console.log(split1);
+
+
+//Q10
+var split2= 'learn JavaScript';
+var split3 = split2.split(" ");
+console.log(split3);
+
+//Q11
+
+let mediasocial = "Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon";
+let media = mediasocial.split(',');
+console.log(media);
+
+//Q12
+let replaced = "learn Javascript";
+console.log(replaced.replace("learn Javascript","learn Javascript to 30 Days Of Python "));
+
+//Q13
+let  character = 'learn Javascript'
+console.log(character.length);
+console.log( character.charAt(15)) ;
+
+//Q14
+let character1 = 'learn Javascript'
+console.log(character1.charCodeAt(3)) 
+
+
+//Q15
+var position = 'learn Javascript';
+console.log(position.indexOf('a'));
+
+
+//Q16
+var position1 = 'learn Javascript';
+console.log(position1.lastIndexOf('a'));
+
+
+//Q17
+var position2 = "You cannot end a sentence with because because because is a conjunction";
+console.log(position2.indexOf("because"));
+
+
+
+//Q18
+var position3 = "You cannot end a sentence with because because because is a conjunction";
+console.log(position3.lastIndexOf("because"));
+
+
+//Q19
+var searchh = "You cannot ";
+console.log(searchh.search("because"));
+
+
+//Q20
+var removespace = " New";
+console.log(removespace);
+console.log(removespace.trim(" "));
+
+
+
+
+ //Q21
+ var start = 'learn Javascript';
+ console.log(start.startsWith("lea"));
+ console.log(start.startsWith("java"));
+
+
+ //Q22
+ 
+ var end = 'learn Javascript';
+ console.log(end.endsWith("lea"));
+ console.log(end.endsWith("Javascript"));
+
+ //Q23
+ let matchh = "learn javascript"
+ console.log(matchh.match("a"));
+
+ //Q24
+ let concats = 'learn javascript'
+ console.log(concats.concat(" 30 Days Of", " JavaScript"));
+
+ //Q25
+ let repeats= 'learn javascript'
+ console.log(repeats.repeat(2));
+ 
